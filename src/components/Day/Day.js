fda9f55cce7a61fc08a106b40eabfce4a6956dfb
MihaylovRoman <ro-man700@mail.ru>
2023-12-10 13:09:37 +0500

    update

--- src/components/Day/Day.js
+++ src/components/Day/Day.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import {useState, useEffect} from 'react'
-// import Task from './components/Task/Task';
+import { useState, useEffect } from 'react'
+import Task from '../Task/Task';
 import './style.css'
 const Day = () => {
     const [value, setValue] = useState('')
@@ -14,24 +14,38 @@ const Day = () => {
     }
 
     const deleteTask = (id) => {
-        const list = [...task]
-        list.splice(id, 1)
-        setTask(list)
+        setTask([...task.filter((todo) => todo.id !== id)])
     }
 
     useEffect(() => {
         localStorage.setItem('todos', JSON.stringify([...task]))
     }, [task])
 
-
     return (
         <div className="wrapperDay">
-
-            <form className='formTodo'>
+            <div className='taskManager'>
+                <div className='Greetings'>
+                    <h2>Доброе утро, username</h2>
+                    <h2>Чем ты будешь заниматься сегодня?</h2>
+                </div>
+                <div>
+                    <div className='calendarDay'>
+                        <h3>SUN</h3>
+                        <h2>10</h2>
+                        <h3>December</h3>
+                    </div>
+                </div>
+                <div className='taskWindow'>
+                    {
+                        task ? task.map((todo, index) => <Task todo={todo} key={index} deleteTask={deleteTask} />) : ''
+                    }
+                </div>
+            </div>
+            <form onSubmit={(e) => onSubmitForm(e)} className='formTodo'>
                 <div className='inputValueWrapper'>
-                    <img className='plusButton' src='../../image/plus.png' alt='plus'/>
-                    <input type='text' value={value} onChange={(e) => setValue(e.target.value)} />
-                    <img className='submitButton' type='submit' onClick={onSubmitForm} src='../../image/sendTask.png' alt='enterTask'/>
+                    <img className='plusButton' src='../../image/plus.png' alt='plus' />
+                    <input type='text' placeholder='Добавить задачу' value={value} onChange={(e) => setValue(e.target.value)} />
+                    <img className='submitButton' onClick={onSubmitForm} src='../../image/sendTask.png' alt='enterTask' />
                 </div>
             </form>
         </div>
@@ -40,4 +54,3 @@ const Day = () => {
 
 export default Day
 
-// task ? task.map((x, index) => <Task value={x} key={index} deleteTask={deleteTask} />) : ''
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,16 +1,17 @@
 import React, { useState } from 'react'
 import './style.css'
-const Task = ({ value, index, deleteTask }) => {
-
+const Task = ({ todo, deleteTask }) => {
     const [active, setActive] = useState(false)
-
+    
     return (
         <div className={`taskWrapper ${active ? 'completed' : ''}`}>
             <div className='taskText'>
-                <input className='taskCheck' type='radio' onClick={() => setActive(!active)} />
-                <p>{value ? value : 'Задача не установлена'}</p>
+                <input className='taskCheck' type='radio' onClick={() => setActive(!active)
+                } />
+                <p>{todo ? todo : 'Задача не установлена'}</p>
             </div>
-            <img src='../../image/trash.png' onClick={() => deleteTask(index)} alt='qwe' />
+            <img src='../../image/trash.png' onClick={() => {deleteTask(todo.id)
+            console.log(todo.id)}} alt='close' />
         </div>
     )
 }
